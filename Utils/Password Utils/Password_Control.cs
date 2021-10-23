058b243d63a547583a9fa8490c143bc4d39bb6f8
Oren Pildus <pildus@gmail.com>
2021-10-23 23:43:32 +0300

    OrenPildusDesigns
    Mid Term

---
+++ Utils/Password Utils/Password_Control.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Utils
+{
+    static class Constants
+    {
+        public const int _passMinLength = 6;
+        public const int _passMaxLength = 12;
+    }
+
+
+    public class Password_Control
+    {
+        // Validation of password length, charachters(Uppercase , numeric, and symbol)
+        public static bool Validate_Password_Params(string pass)
+        {
+            //checking password length
+            if (pass.Length < Constants._passMinLength || pass.Length > Constants._passMaxLength)
+                return false;
+
+            if (!pass.Any(char.IsUpper))
+                return false;
+
+            return true;
+        }
+    }
+}
---
+++ tempUtils/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Utils;
+
+namespace tempUtils
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pass = "Lev$iha2016!";
+
+            bool p = Password_Control.Validate_Password_Params(pass);
+            Console.WriteLine(p.ToString());
+
+            Console.WriteLine(Password_Control.Encrypt_Password(pass));
+
+            Console.WriteLine(Password_Control.Encrypt_Password(pass)== Password_Control.Encrypt_Password("Lev$iha2016!"));
+
+        }
+    }
+}
